0c0b9a6d8a689aeccc0bcfa7d81f3f30dfc03f75
Rune Harlyk <rune@harlyk.dk>
2023-07-01 17:32:44 +0200

    🌠 Simplifying code for processes

--- src/components/app/taskManager/processes/+page.svelte
+++ src/components/app/taskManager/processes/+page.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import type { Process } from '../../../../models';
-	import { processes } from '../../../../store';
+	import { processes } from '../../../../stores';
 	import Icon from '../../../icon.svelte';
 
 	export let process: Process
+
+	$: apps = $processes.filter((p) => !p.background && p.parent == process.parent)
+	$: backgroundApps = $processes.filter((p) => p.background && p.parent == process.parent)
 </script>
 
 <div class="p-4">
-	<h2 class="mb-4">Apps({$processes.length})</h2>
+	<h2 class="mb-4">Apps({apps.length})</h2>
 	<ul>
-		{#each $processes.filter((p) => !p.background && p.parent == process.parent) as app}
+		{#each apps as app}
 			<li class="flex gap-4"><Icon type={app.icon} />{app.type}</li>
 		{/each}
 	</ul>
 
-	<h2 class="mt-8 mb-4">Background processes({$processes.filter((p) => p.background).length})</h2>
+	<h2 class="mt-8 mb-4">Background processes({backgroundApps.length})</h2>
 	<ul>
-		{#each $processes.filter((p) => p.background) as process}
+		{#each backgroundApps as process}
 			<li class="flex gap-4"><Icon type={process.icon} />{process.type}</li>
 		{/each}
 	</ul>
