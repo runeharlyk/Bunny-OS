fde739c53abf6596da1c800f1e7af65907326a8c
Rune Harlyk <rune@harlyk.dk>
2023-07-02 20:11:33 +0200

    📦 Moves humanFileSize function to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,32 @@ export const arrayBufferToBase64Img = (buffer: ArrayBuffer):string => {
 }
 
 export const arrayBufferToBase64 = (buffer: ArrayBuffer):string => {
-    var binary = '';
-    var bytes = new Uint8Array(buffer);
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
         binary += String.fromCharCode(bytes[i]);
     }
     return window.btoa(binary);
 }
+
+export const humanFileSize = (bytes: number, si = false, dp = 1) => {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+    return bytes.toFixed(dp) + ' ' + units[u];
+};
