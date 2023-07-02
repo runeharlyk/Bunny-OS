e06935dd413e190773789763e647d3222befd0b6
Rune Harlyk <rune@harlyk.dk>
2023-07-02 20:13:01 +0200

    🗂 Gives FileExplore a facelift with navigateable history

--- src/components/app/fileExplore/+page.svelte
+++ src/components/app/fileExplore/+page.svelte
@@ -2,14 +2,19 @@
 	import App from '../+page.svelte';
 	import type { Process } from '../../../models';
 	import Icon from '../../icon.svelte';
-	import { Printer } from 'svelte-heros';
+	import { Printer, ComputerDesktop } from 'svelte-heros';
 	import { onMount } from 'svelte';
+	import { humanFileSize } from '$lib/utils';
 	export let process: Process;
 
-	let freeSpace: string | undefined;
-	let usedSpace: string | undefined;
+	let historyIndex = 0;
+	let history: FileSystemDirectoryHandle[] = [];
+
+	let freeSpace = 0;
+	let usedSpace = 0;
+	let totalSpace = 0;
 	let w = window as any;
-	let entries: string[] = [];
+	let entries:Array<FileSystemDirectoryHandle> = [];
 	let usbDevices: USBDevice[] = [];
 
 	onMount(async () => {
@@ -21,15 +26,14 @@
 		let directoryHandle = await storage.getDirectory();
 		let persistent = await storage.persist();
 		let estimate = await storage.estimate();
-		freeSpace = humanFileSize(estimate.quota ?? 0, true);
-		usedSpace = humanFileSize(estimate.usage ?? 0, true);
+		totalSpace = estimate.quota ?? 0
+		usedSpace = estimate.usage ?? 0
+		freeSpace = totalSpace - usedSpace
 
 		if (!persistent) {
 			console.log('Could not get persistent storage');
 			return;
 		}
-		console.log(storage);
-		console.log(directoryHandle);
 
 		navigator.usb?.addEventListener('connect', (event: any) => {
 			console.log(event);
@@ -41,19 +45,9 @@
 		getUsbDevices();
 	});
 
-
 	const mount = async () => {
-		const dirHandle = await w.showDirectoryPicker();
-		let tempEntries = [];
-		for await (const [key, value] of dirHandle.entries()) {
-			tempEntries.push(key);
-		}
-		entries = tempEntries;
-		console.log(entries);
-
-		for await (const val of dirHandle.keys()) {
-			console.log(val);
-		}
+		const folder = await w.showDirectoryPicker();	
+		await openFolder(folder, true);
 	};
 
 	const getUsbDevices = async () => {
@@ -63,26 +57,95 @@
 	const addNewUsbDevices = async () => {
 		await navigator.usb?.requestDevice({ filters: [] });
 	};
+
+	const historyBack = () => {
+		historyIndex--;
+		openFolder(history[historyIndex])
+		//updateBrowserContext();
+	};
+
+	const historyForward = () => {
+		historyIndex++;
+		openFolder(history[historyIndex])
+		//updateBrowserContext();
+	};
+
+	const openFolder = async (folder: FileSystemDirectoryHandle, addToHistory:boolean = false) => {
+		if(addToHistory) {
+			history.push(folder);
+			historyIndex++
+		}
+		console.log(history);
+		
+		let tempEntries = [];
+		for await (const [, value] of folder.entries()) {
+			tempEntries.push(value);
+		}
+		
+		tempEntries.sort((a, b) => a.constructor.name.localeCompare(b.constructor.name))
+		
+		entries = tempEntries;
+	}
 </script>
 
 <App on:close {process}>
 	<div slot="icon"><Icon type="Folder" /></div>
 	<span slot="app-name">{process.name}</span>
 
-	<div slot="content" class="flex h-full px-4 flex-col">
-		used space:{usedSpace}<br />
-		free space:{freeSpace}<br />
-		<div>
-			<button on:click={mount}> Open Files </button>
-			{#each entries as entry}
-				<li class="flex gap-4">{entry}</li>
-			{/each}
+	<div slot="content" class="flex h-full px-4 flex-col bg-gray-800">
+		<div class="w-full">
+			<div class="py-4 flex gap-4">
+				<button
+					on:click={historyBack}
+					disabled={historyIndex === 0}
+					class="round-full"
+					class:text-gray-500={historyIndex === 0}>🡨</button
+				>
+				<button
+					on:click={historyForward}
+					disabled={historyIndex === history.length}
+					class:text-gray-500={historyIndex === history.length}>🡪</button
+				>
+				<input class="w-3/4 bg-gray-800 border border-gray-600 p-1" placeholder=" > Bunny OS > data"/>
+				<input class="w-1/4 bg-gray-800 border border-gray-600 p-1" placeholder=" Search"/>
+			</div>
 		</div>
-		<div>
-			<button on:click={addNewUsbDevices}>Add new Usb Device</button>
-			{#each usbDevices as device}
-				<li class="flex gap-4"><Printer />{device.productName} - {device.manufacturerName}</li>
-			{/each}
+
+		<div class="flex h-full">
+			<ul class="flex flex-col w-1/4">
+				<div>Devices</div>
+				{#each usbDevices as device}
+					<li class="flex gap-4">
+						<Printer />
+						<div class="w-full text-ellipsis overflow-hidden">{device.productName} - {device.manufacturerName}</div>
+					</li>
+				{/each}
+			</ul>
+			<div class="w-3/4 border-gray-600 border-l p-4">
+				<button class="flex items-center gap-2 hover:bg-slate-600 p-3">
+					<div><ComputerDesktop /></div>
+					<div class="flex flex-col items-start">
+						<div>Bunny storage</div>
+						<div class="flex h-5 w-full bg-slate-500">
+							<div class="bg-blue-500" style="width: {(usedSpace-freeSpace) / usedSpace*100}%;"></div>
+							<div class=""></div>
+						</div>
+						<div>{humanFileSize(freeSpace, true)} free of {humanFileSize(totalSpace, true)}</div>
+					</div>
+				</button>
+				<div>
+					<button on:click={mount}> Open Files </button>
+					{#each entries as entry}
+						<button class="flex gap-4" on:dblclick={() => openFolder(entry, true)}>
+							<Icon type="{entry.kind === "directory" ? "Folder" : "Code"}" />
+							{entry.name}
+						</button>
+					{/each}
+				</div>
+				<div>
+					<button on:click={addNewUsbDevices}>Add new Usb Device</button>
+				</div>
+			</div>
 		</div>
 	</div>
 </App>
