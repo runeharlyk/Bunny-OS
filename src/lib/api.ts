e7c436f04319750a79390aea7300020953c216c4
Rune Harlyk <rune@harlyk.dk>
2023-07-02 22:00:09 +0200

    🧤 Makes intercepted reponse untouched

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,9 +5,10 @@ export const registerFetchIntercept = () => {
     window.fetch = async (...args) => {
         const [resource, config] = args
         const response = await originalFetch(resource, config)
+        const responseClone = response.clone();
         const sendSize = new Blob(["" + config?.headers?.toString() + config?.body?.toString()]).size
-        const contentLength = Number(response.headers.get("content-length"))
-        const responseSize = contentLength !== 0 ? contentLength : (await (response.blob())).size
+        const contentLength = Number(responseClone.headers.get("content-length"))
+        const responseSize = contentLength > 0 ? contentLength : (await (responseClone.blob())).size
         
         SendSize.update((size) => size + sendSize)
         ReceivedSize.update((size) => size + responseSize)
